9ec7152978c5803c3671601816a2679e3e5613a0
Wesley Ellis <tahnok@gmail.com>
2015-05-04 22:21:12 -0400

    add some tones, queued play back

--- app/src/main/java/me/tahnok/blueboxer/BlueBoxActivity.java
+++ app/src/main/java/me/tahnok/blueboxer/BlueBoxActivity.java
@@ -1,15 +1,24 @@
 package me.tahnok.blueboxer;
 
 import android.media.MediaPlayer;
+import android.support.annotation.RawRes;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.CountDownLatch;
+
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import butterknife.OnClick;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.schedulers.Schedulers;
 
 
 public class BlueBoxActivity extends ActionBarActivity {
@@ -17,15 +26,41 @@ public class BlueBoxActivity extends ActionBarActivity {
     public static final int STAR = 10;
     public static final int POUND = 11;
     public static final int TONE_2600 = 12;
+    private static final String LOG_TAG = BlueBoxActivity.class.getSimpleName();
+
     //VIEWS
     @InjectView(R.id.button_1) Button button_1;
 
+    final ArrayBlockingQueue<Integer> integerQueue = new ArrayBlockingQueue<>(100);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blue_box);
         ButterKnife.inject(this);
+
+        Observable<Integer> soundQueue = Observable.create(new Observable.OnSubscribe<Integer>() {
+            @Override
+            public void call(Subscriber<? super Integer> subscriber) {
+                while(true) {
+                    try {
+                        subscriber.onNext(integerQueue.take());
+                    } catch (InterruptedException e) {
+                        subscriber.onError(e);
+                    }
+                }
+            }
+        });
+
+        soundQueue
+                .subscribeOn(Schedulers.io())
+                .observeOn(Schedulers.computation())
+                .subscribe(new Action1<Integer>() {
+                    @Override
+                    public void call(Integer integer) {
+                        blockingPlayTone(integer);
+                    }
+                });
     }
 
 
@@ -55,72 +90,122 @@ public class BlueBoxActivity extends ActionBarActivity {
 
     @OnClick(R.id.button_1)
     void button1Click() {
-        playTone(1);
+        queueTone(1);
     }
 
     @OnClick(R.id.button_2)
     void button2Click() {
-        playTone(2);
+        queueTone(2);
     }
 
     @OnClick(R.id.button_3)
     void button3Click() {
-        playTone(3);
+        queueTone(3);
     }
 
     @OnClick(R.id.button_4)
     void button4Click() {
-        playTone(4);
+        queueTone(4);
     }
 
     @OnClick(R.id.button_5)
     void button5Click() {
-        playTone(5);
+        queueTone(5);
     }
 
     @OnClick(R.id.button_6)
     void button6Click() {
-        playTone(6);
+        queueTone(6);
     }
 
     @OnClick(R.id.button_7)
     void button7Click() {
-        playTone(7);
+        queueTone(7);
     }
 
     @OnClick(R.id.button_8)
     void button8Click() {
-        playTone(8);
+        queueTone(8);
     }
 
     @OnClick(R.id.button_9)
     void button9Click() {
-        playTone(9);
+        queueTone(9);
     }
 
     @OnClick(R.id.button_0)
     void button0Click() {
-        playTone(0);
+        queueTone(0);
     }
 
     @OnClick(R.id.button_star)
     void buttonStarClick() {
-        playTone(STAR);
+        queueTone(STAR);
     }
 
     @OnClick(R.id.button_pound)
     void buttonPoudClick() {
-        playTone(POUND);
+        queueTone(POUND);
     }
 
     @OnClick(R.id.button_2600)
     void button2600Click() {
-        playTone(TONE_2600);
+        queueTone(TONE_2600);
     }
     // MEDIA PLAYING
-    private void playTone(int i) {
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.tone2600);
+    private void queueTone(int i) {
+        try {
+            integerQueue.put(i);
+        } catch (InterruptedException e) {
+            Log.e(LOG_TAG, "????????", e);
+        }
+    }
+
+    private void blockingPlayTone(int tone_id) {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, getToneId(tone_id));
+        final CountDownLatch latch = new CountDownLatch(1);
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                latch.countDown();
+            }
+        });
         mediaPlayer.start();
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            Log.e(LOG_TAG, "couldn't wait", e);
+        }
+        mediaPlayer.release();
+    }
+
+    private int getToneId(int tone_id) {
+        switch(tone_id) {
+            case TONE_2600:
+                return R.raw.tone2600;
+            case 0:
+                return R.raw.tone_0;
+            case 1:
+                return R.raw.tone_1;
+            case 2:
+                return R.raw.tone_2;
+            case 3:
+                return R.raw.tone_3;
+            case 4:
+                return R.raw.tone_4;
+            case 5:
+                return R.raw.tone_5;
+            case 6:
+                return R.raw.tone_6;
+            case 7:
+                return R.raw.tone_7;
+            case 8:
+                return R.raw.tone_8;
+            case 9:
+                return R.raw.tone_9;
+            default:
+                return R.raw.tone2600;
+        }
     }
 
 }
